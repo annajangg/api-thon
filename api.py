e63224c26f31f52d9a09bbb0aeb5a50c980e6aca
Marc Lewis <marc8@live.unc.edu>
2021-11-09 15:35:20 -0500

    stuff

--- api.py
+++ api.py
@@ -1,14 +1,45 @@
-from requests import api, models
+from requests import get
+from requests.models import Response
 from random import choice
 
-Response: type = models.Response  # this is the type of a response object
+# 1. Starting small
+# https://boredapi.com/api/activity/, returns a random activity for you to do.
 
-res: Response = api.get("https://api.le-systeme-solaire.net/rest/bodies/")
+bored_api_response: Response = get("https://boredapi.com/api/activity/")
 
-data: list[dict[str: str]] = res.json()["bodies"]
+# response codes
+# 200 means everything went ok.
+# 400 errors are errors on your end.
+# 500 errors are errors on the servers end.
 
-print(choice(data))
-# for planet in data:
-#     if planet["englishName"] and not planet["isPlanet"]:
-        
-#         print("Planet: " + planet["englishName"] + ", id: " + planet["id"])
+print(bored_api_response.status_code) 
+
+# json == JavaScript Object Notation.
+# just a large dictionary with str keys and variable values.
+
+print(bored_api_response.json())
+
+# other fields
+bored_api_response.url  # the url that was sent with the request (more interesting when params are involved)
+
+# 2. Requests with params
+# https://api.agify.io, returns average age for people with a given name (passed as a parameter)
+# 
+
+# name = "io"
+# space_res = get(f"https://api.le-systeme-solaire.net/rest/bodies/{name}")
+# space_data = space_res.json()
+# print(space_data)
+
+space_res = get("https://api.le-systeme-solaire.net/rest/bodies/", params={"order": "eccentricity,asc"})
+data = space_res.json()["bodies"]
+for s in data:
+    if str(s["eccentricity"]) != "":
+        print(s["id"], end="")
+        print(f": {s['englishName']} {str(s['eccentricity'])}", end="")
+        print()
+
+# refined_res = get("https://api.le-systeme-solaire.net/rest/bodies/io")
+# print(refined_res.json())
+
+# 3. Requests with an API key
--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask.templating import Environment
 from livereload import Server
+from requests import get
+from requests.models import Response
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+async def index():
     if request.method == "POST":
 
         object_name = request.form['object_name']
 
+        # TODO: make your api request for the object the user specified
+        res: Response = get("https://api.agify.io", params={"name": "Kaki"})
+        data = res.json()
+
         if object_name == '':
             return render_template("index.html")
 
-        return render_template("result.html", object_name=object_name)
+        return render_template("result.html", object_name=data["count"])
     return render_template('index.html')
 
 
