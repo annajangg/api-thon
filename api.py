7a12dd37ae8ca26515153d61f6b354015207cb76
Marc Lewis <marc8@live.unc.edu>
2021-11-12 16:22:45 -0500

    API! API. What is an API?

--- api.py
+++ api.py
@@ -1,35 +1,22 @@
-from requests import get
-from requests.models import Response
+from requests import get  # This function is how we will make requests to a server.
+from requests.models import Response  # This is the type of a Response object.
+
 
 # 1. Starting small
-# https://boredapi.com/api/activity/, returns a random activity for you to do.
+# Endpoint: https://boredapi.com/api/activity/, returns a random activity for you to do.
 
-bored_api_response: Response = get("https://boredapi.com/api/activity/")
 
-# response codes
-# 200 means everything went ok.
-# 400 errors are errors on your end.
-# 500 errors are errors on the servers end.
-
-print(bored_api_response.status_code) 
-
-# json == JavaScript Object Notation.
-# just a large dictionary with str keys and variable values.
-
-print(bored_api_response.json())
-
-# other fields
-bored_api_response.url  # the url that was sent with the request (more interesting when params are involved)
 
 # 2. Requests with params
-# https://api.agify.io, returns average age for people with a given name (passed as a parameter)
-# 
+# Endpoint: https://api.agify.io, returns average age for people with a given name (passed as a parameter)
 
 def grab_url(planet: str, is_planet: bool) -> str:
-    """Helper function to populate url variable in app.py. These files live in our 'static' folder."""
+    """Helper function to populate url variable in app.py. The file paths we build relate to files in our 'static' directory."""
     if is_planet:
         return f"/{planet.lower()}.jpg"
     else:
         return f"/space.jpg"
 
 # 3. Requests with an API key
+# TODO: Sign up for an API key: https://api.nasa.gov/
+# Endpoint: https://api.nasa.gov/planetary/apod, returns the astronomy picture of the day! 
--- requests-ex.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-requests.get("https://api.agify.io", params={"name": "Marc"})
-# or
-requests.get("https://api.agify.io/?name=Marc")
-
-name: str = input("What is your name?")
-requests.get("https://api.agify.io", params={"name": name})
